81af2b7d75526ea48b9f171f085a31e25ac1c8ae
Henri <lakebd@web.de>
2023-06-22 16:49:28 +0200

    bug fix

--- test_hpc.py
+++ test_hpc.py
@@ -1,28 +1,24 @@
 from diffusers import DiffusionPipeline
 import torch, os.path
-import tomesd
+from tomesd import tomesd
 from pytorch_fid.src.fid.fid_score import calc_fid_given_lists
 from time import time
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
-'''
-pipeline = DiffusionPipeline.from_pretrained("/gpfs/project/hebal100/ba-code/pipelines/SD-v1-5")
-pipeline.to(device)
+pipeline = DiffusionPipeline.from_pretrained("/gpfs/project/hebal100/ba-code/pipelines/SD-v1-5").to("cuda")
 pipeline.enable_attention_slicing()
 
 prompt = "photograph of an astronaut riding a horse"
 seed = 555035
 
-image_c1 = pipeline(prompt, 768, 768, generator=torch.Generator().manual_seed(seed)).images[0]
-image_c2 = pipeline(prompt, 768, 768, generator=torch.Generator().manual_seed(seed)).images[0]
+image_c1 = pipeline(prompt, 768, 768, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
+image_c2 = pipeline(prompt, 768, 768, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
 tomesd.apply_patch(pipeline, 0.5)
-image_m1 = pipeline(prompt, 768, 768, generator=torch.Generator().manual_seed(seed)).images[0]
-image_m2 = pipeline(prompt, 768, 768, generator=torch.Generator().manual_seed(seed)).images[0]
+image_m1 = pipeline(prompt, 768, 768, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
+image_m2 = pipeline(prompt, 768, 768, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
 tomesd.remove_patch(pipeline)
 
 fid1 = calc_fid_given_lists(image_c1, image_c2)
 fid2 = calc_fid_given_lists(image_m1, image_m2)
 
 print(fid1, fid2)
-'''
+
