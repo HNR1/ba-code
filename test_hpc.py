8c2841f67f9c49a52b26fe3cfe66c55cf79949ad
Henri <lakebd@web.de>
2023-06-22 14:19:27 +0200

    change import

--- test_hpc.py
+++ test_hpc.py
@@ -1,10 +1,12 @@
 from diffusers import DiffusionPipeline
 import torch, os.path
 import tomesd
-import pytorch_fid.src.fid.fid_score as fid_score
+from pytorch_fid.src.fid.fid_score import calc_fid_given_lists
 from time import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+'''
 pipeline = DiffusionPipeline.from_pretrained("/gpfs/project/hebal100/ba-code/pipelines/SD-v1-5")
 pipeline.to(device)
 pipeline.enable_attention_slicing()
@@ -19,7 +21,8 @@ image_m1 = pipeline(prompt, 768, 768, generator=torch.Generator().manual_seed(se
 image_m2 = pipeline(prompt, 768, 768, generator=torch.Generator().manual_seed(seed)).images[0]
 tomesd.remove_patch(pipeline)
 
-fid1 = fid_score.calc_fid_given_lists(image_c1, image_c2)
-fid2 = fid_score.calc_fid_given_lists(image_m1, image_m2)
+fid1 = calc_fid_given_lists(image_c1, image_c2)
+fid2 = calc_fid_given_lists(image_m1, image_m2)
 
 print(fid1, fid2)
+'''
