d1ac4b77be5351cdcb8b2a1574b4906da993609f
Henri <lakebd@web.de>
2023-07-12 16:37:37 +0200

    change generator

--- tests/test_hpc.py
+++ tests/test_hpc.py
@@ -11,11 +11,11 @@ prompt = "photograph of an astronaut riding a horse"
 seed = 555035
 
 x, y = 768, 768
-image_c1 = pipeline(prompt, x, y, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
-image_c2 = pipeline(prompt, x, y, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
+image_c1 = pipeline(prompt, x, y, generator=torch.Generator().manual_seed(seed)).images[0]
+image_c2 = pipeline(prompt, x, y, generator=torch.Generator().manual_seed(seed)).images[0]
 tomesd.apply_patch(pipeline, 0.5)
-image_m1 = pipeline(prompt, x, y, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
-image_m2 = pipeline(prompt, x, y, generator=torch.Generator(device='cuda').manual_seed(seed)).images[0]
+image_m1 = pipeline(prompt, x, y, generator=torch.Generator().manual_seed(seed)).images[0]
+image_m2 = pipeline(prompt, x, y, generator=torch.Generator().manual_seed(seed)).images[0]
 tomesd.remove_patch(pipeline)
 
 fid1 = calc_fid_given_lists(image_c1, image_c2)
