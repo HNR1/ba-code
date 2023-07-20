b0fe0891356529bd083505eec4feb682209deb28
Henri <lakebd@web.de>
2023-07-20 13:25:13 +0200

    improvements to test_parallel

--- scripts/load_models.py
+++ scripts/load_models.py
@@ -1,14 +1,17 @@
-from diffusers import DiffusionPipeline
-import sys
+from diffusers import DiffusionPipeline, DDPMParallelScheduler, StableDiffusionParadigmsPipeline
+import sys, torch
 
 # python load_models.py model_id_from_huggingface model_name_to_save
 model_id = sys.argv[1]
 
 # Download files ahead of time
-pipeline = DiffusionPipeline.from_pretrained(model_id)
+#pipeline = DiffusionPipeline.from_pretrained(model_id)
+scheduler = DDPMParallelScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionParadigmsPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+
 
 # Save files to a specified directory
 pipeline_path = "/Users/henri/Info/ba-code/pipelines/"+sys.argv[2]
-pipeline.save_pretrained(pipeline_path)
+pipe.save_pretrained(pipeline_path)
 
 
--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -1,88 +1,31 @@
-"""
-import sys       
+import sys, os
 sys.path.insert(1, '/gpfs/project/hebal100/ba-code/libs')
-from diffusers import DiffusionPipeline
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7,8,9'
 import torch, numpy as np
-import torch.nn as nn
-#from tomesd import tomesd
-#from pytorch_fid.src.fid.fid_score import calc_fid_given_lists
+from diffusers import StableDiffusionParadigmsPipeline
 
 assert torch.cuda.is_available()
-pipeline = DiffusionPipeline.from_pretrained("/gpfs/project/hebal100/ba-code/pipelines/SD-v1-5").to('cuda')
+pipeline = StableDiffusionParadigmsPipeline.from_pretrained("/gpfs/project/hebal100/ba-code/pipelines/SD-v1-5-parallel").to('cuda')
 pipeline.enable_attention_slicing()
 
-seeds = np.random.randint(1, 2000000000, 5)
+ngpu, batch_per_device = 10, 5
+
+pipeline.wrapped_unet = torch.nn.DistributedDataParallel(pipeline.unet, device_ids=[0,1,2,3,4,5,6,7,8,9])
 
 filename = "/gpfs/project/hebal100/ba-code/data/prompts.txt"
 prompts = []
 with open(filename, 'r', encoding='UTF-8') as file:
     while line := file.readline():
         prompts.append(line.rstrip())
-
-x, y = 768, 768
-images = []
-images_tome = []
-
-num_images = 5
-
-for i in range(num_images):
-    images.append(pipeline(prompts[i], x, y, generator=torch.Generator().manual_seed(seeds[i])).images[0])
-
-print(len(images))
-#tomesd.apply_patch(pipeline, 0.5)
-
-#tomesd.remove_patch(pipeline)
-
-#fid = calc_fid_given_lists(images, images_tome)
-
-#print(fid)
-"""
-#
-#
-#
-#
-#
-#
-import sys
-sys.path.insert(1, '/gpfs/project/hebal100/ba-code/libs')
-from diffusers import DiffusionPipeline
-import torch, numpy as np
-import torch.nn as nn
-
-assert torch.cuda.is_available()
-print(torch.cuda.device_count())
-num_gpus = torch.cuda.device_count()
-if num_gpus < 1:
-    raise RuntimeError("No GPUs available for parallel execution.")
-
-pipeline = DiffusionPipeline.from_pretrained("/gpfs/project/hebal100/ba-code/pipelines/SD-v1-5")
-
-# Enable attention slicing for parallel execution
-pipeline.enable_attention_slicing()
-
-seeds = np.random.randint(1, 2000000000, num_gpus)
-
-filename = "/gpfs/project/hebal100/ba-code/data/prompts.txt"
-prompts = []
-with open(filename, 'r', encoding='UTF-8') as file:
-    while line := file.readline():
-        prompts.append(line.rstrip())
+seeds = np.random.randint(1, 2000000000, len(prompts))
 
 x, y = 768, 768
 images = []
 
-for i in range(num_gpus):
-    # Set the current GPU
-    torch.cuda.set_device(i)
+for i in range(len(prompts)):
 
-    # Move the pipeline to the current GPU
-    pipeline.to(torch.device('cuda', i))
-
-    # Wrap the pipeline with DataParallel
-    pipeline_parallel = nn.DataParallel(pipeline)
-
-    # Generate image on the current GPU
-    image = pipeline_parallel(prompts[i], x, y, generator=torch.Generator().manual_seed(seeds[i])).images[0]
+    image = pipeline(prompts[i], x, y, parallel=ngpu * batch_per_device,
+                     generator=torch.Generator().manual_seed(seeds[i].item())).images[0]
 
     images.append(image)
 
