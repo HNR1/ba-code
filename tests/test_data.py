d4b7b8e69476c52b0e99b933e0aefa46ef3f272e
Henri <lakebd@web.de>
2023-07-24 12:14:08 +0200

    improvements to test_data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,32 +1,39 @@
 import sys       
-sys.path.insert('/gpfs/project/hebal100/ba-code/libs')
-sys.path.insert('/gpfs/project/hebal100/ba-code')
+sys.path.insert(1, '/gpfs/project/hebal100/ba-code/libs')
+sys.path.insert(2, '/gpfs/project/hebal100/ba-code')
 from diffusers import DiffusionPipeline
-from datasets import DatasetDict
 import torch, numpy as np
+import pandas as pd
 from tomesd import tomesd
-from pytorch_fid.src.fid.fid_score import calc_fid_given_lists
 
 assert torch.cuda.is_available()
 pipeline = DiffusionPipeline.from_pretrained("pipelines/SD-v1-5").to('cuda')
 pipeline.enable_attention_slicing()
 
-all_prompts = DatasetDict.from_parquet("data/prompts_hf.parquet")
-num_prompts = 10
-prompts = all_prompts[:num_prompts].get('Prompt')
+all_prompts = pd.read_csv('data/prompts.csv')['colummn'].values
+sample_size = 10
+idcs = np.random.randint(0, len(all_prompts), sample_size)
+prompts = all_prompts[idcs]
+print(prompts[0])
 seeds = np.random.randint(0, 4294967295, len(prompts))
 
 num_imgs = 2
-assert num_imgs <= num_prompts
+assert num_imgs <= sample_size
 images = []
 
-def cut_prompt(prompt, length, char):
-    idx = prompt[:length].rfind(char)
+def cut_prompt(prompt, max_len=300, char=','):
+    if len(prompt) <= max_len:
+        return prompt
+    idx = prompt[:max_len].rfind(char)
+    # catch if char isn't used
+    if idx <= 0:
+        idx = prompt[:max_len].rfind(' ')
     return prompt[:idx]
 
 x, y = 768, 768
+# TODO: measure diffusion time
 for i in range(num_imgs):
-    prompt = cut_prompt(prompts[i], 300, ',')
+    prompt = cut_prompt(prompts[i])
     print(prompt)
     image = pipeline(prompt, x, y, generator=torch.Generator().manual_seed(seeds[i].item())).images[0]
     images.append(image)
