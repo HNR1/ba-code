a8ffa5e19531ed960bffbafe16a892f2b6d7d060
Henri <lakebd@web.de>
2023-07-29 11:12:10 +0200

    bug fix

--- src/logging/gen_perf_log.py
+++ src/logging/gen_perf_log.py
@@ -5,12 +5,17 @@ from libs.pytorch_fid.src.fid.fid_score import calculate_fid_given_paths
 
 PATH = sys.argv[1] #'data/run2_768x768'
 directories = ['images_0', 'images_10', 'images_20', 'images_30', 'images_40', 'images_50', 'images_60']
-fid_values = []
+m_vols = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+perf_values = []
 
-for dir in directories:
+for v, dir in zip(m_vols, directories):
     fid = calculate_fid_given_paths((f'/gpfs/scratch/hebal100/{PATH}/images_0', 
                                      f'/gpfs/scratch/hebal100/{PATH}/{dir}'), batch_size=50, device='cuda', dims=2048)
-    fid_values.append(fid)
+    df = pd.read_csv(f'/gpfs/scratch/hebal100/{PATH}/img_log.csv')
+    df = df[df.m_vol == v]
+    avg_time = df['time'].values.mean()
+    perf_values.append([v, fid, avg_time])
 
-fid_log = pd.DataFrame(fid_values, columns=['fid'])
-fid_log.to_csv(f'/gpfs/scratch/hebal100/{PATH}/fid_log.csv', index=False)
+perf_log = pd.DataFrame(perf_values, columns=['m_vol', 'fid', 'time'])
+perf_log.to_csv(f'/gpfs/scratch/hebal100/{PATH}/performance_log.csv', index=False)
+
